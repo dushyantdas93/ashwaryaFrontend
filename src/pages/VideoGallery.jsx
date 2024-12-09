6e24dacb4d8761d07ee67800c68428b1d7289a15
DUSHYANT DAS MANIKPURI <149514159+dushyantdas93@users.noreply.github.com>
2024-12-10 01:46:49 +0530

    Update VideoGallery.jsx
    update llink

--- src/pages/VideoGallery.jsx
+++ src/pages/VideoGallery.jsx
@@ -7,7 +7,7 @@ const VideoGallery = () => {
 <h1 className='text-2xl font-bold capitalize py-2'>video Gallery</h1>
 
 <div className="flex flex-wrap p-2 gap-2 lg:justify-center  ">
-  {["https://youtu.be/LXb3EKWsInQ?si=cCl9pZvuOYqynKF7","https://youtu.be/7ZP7TWiOrDs?si=wWkxIFaxO2DPL0uy","https://youtu.be/qHxxmWySb6c?si=f2807Su2s3ZSALJ3","https://youtu.be/SJa4Qpkqg-I?si=sdBOJlfScw6CVUrO"].map((item,idx)=>{
+  {["https://youtu.be/Sr1kzDcON38?si=n35iAl5XhoXbxpQC","https://youtu.be/KSiFxXoxbD8?si=Hlh5FC9DZliSLkwm","https://youtu.be/DENYdf0n9wo?si=r9tCkA-69AxxeoU7","https://youtu.be/5b3C3byJqvo?si=cyWhBdEnguMEi9E2"].map((item,idx)=>{
     return <iframe width="560" height="315" src={item} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
    
   })}
