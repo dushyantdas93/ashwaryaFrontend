1569054eb569c9ce8d2ae9c489da0510156b8dd3
DUSHYANT DAS MANIKPURI <149514159+dushyantdas93@users.noreply.github.com>
2024-12-10 01:52:31 +0530

    Update VideoGallery.jsx
    fjggh

--- src/pages/VideoGallery.jsx
+++ src/pages/VideoGallery.jsx
@@ -7,11 +7,14 @@ const VideoGallery = () => {
 <h1 className='text-2xl font-bold capitalize py-2'>video Gallery</h1>
 
 <div className="flex flex-wrap p-2 gap-2 lg:justify-center  ">
-  {["https://youtu.be/Sr1kzDcON38?si=n35iAl5XhoXbxpQC","https://youtu.be/KSiFxXoxbD8?si=Hlh5FC9DZliSLkwm","https://youtu.be/DENYdf0n9wo?si=r9tCkA-69AxxeoU7","https://youtu.be/5b3C3byJqvo?si=cyWhBdEnguMEi9E2"].map((item,idx)=>{
+  {["https://www.youtube.com/embed//Sr1kzDcON38?si=n35iAl5XhoXbxpQC","https://www.youtube.com/embed/KSiFxXoxbD8?si=Hlh5FC9DZliSLkwm","https://www.youtube.com/embed/DENYdf0n9wo?si=r9tCkA-69AxxeoU7","https://www.youtube.com/embed/5b3C3byJqvo?si=cyWhBdEnguMEi9E2"].map((item,idx)=>{
     return <iframe width="560" height="315" src={item} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
    
   })}
+
+{/*   <iframe width="560" height="315" src={item} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
 </div>
+{/*       <iframe width="560" height="315" src="https://www.youtube.com/embed/5b3C3byJqvo?si=cyWhBdEnguMEi9E2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
 </div>
   )
 }
