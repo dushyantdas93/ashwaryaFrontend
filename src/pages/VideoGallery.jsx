067e775e3a7e0d20fff96d54fe14267edf722f91
DUSHYANT DAS MANIKPURI <149514159+dushyantdas93@users.noreply.github.com>
2024-12-10 01:36:47 +0530

    Update VideoGallery.jsx

--- src/pages/VideoGallery.jsx
+++ src/pages/VideoGallery.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+noimport React from 'react'
 
 const VideoGallery = () => {
   return (
@@ -7,7 +7,7 @@ const VideoGallery = () => {
 <h1 className='text-2xl font-bold capitalize py-2'>video Gallery</h1>
 
 <div className="flex flex-wrap p-2 gap-2 lg:justify-center  ">
-  {["https://www.youtube.com/embed/XRSQHkzrQGQ?si=vH0GjubbyWD92ZEI","https://www.youtube.com/embed/XRSQHkzrQGQ?si=vH0GjubbyWD92ZEI"].map((item,idx)=>{
+  {["https://youtu.be/LXb3EKWsInQ?si=cCl9pZvuOYqynKF7","https://youtu.be/7ZP7TWiOrDs?si=wWkxIFaxO2DPL0uy","https://youtu.be/qHxxmWySb6c?si=f2807Su2s3ZSALJ3","https://youtu.be/SJa4Qpkqg-I?si=sdBOJlfScw6CVUrO"].map((item,idx)=>{
     return <iframe width="560" height="315" src={item} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
    
   })}
